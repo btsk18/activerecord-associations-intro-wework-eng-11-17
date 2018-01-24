cdcb4ed18ac8ad905a97e4b2f625fe57bfd46a19
Brandon Leeds <brandonleeds@gmail.com>
2018-01-24 01:49:52 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
-class CreateSongs < ActiveRecord::Migration
+class CreateSongs < ActiveRecord::
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+    end
+  end
 end
